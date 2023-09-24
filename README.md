d9f9ac72f8a10abaf127ce51822a997e72c19b0a
Srevin Saju <srevinsaju@sugarlabs.org>
2023-09-24 19:01:42 +0300

    feat: sync docs for v1.4.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# Togomak Documentation
+
+This is a `Next.js` based documentation for the build orchestration tool
+`togomak` at [srevinsaju/togomak](https://github.com/srevinsaju/togomak)
+
+## Contributing 
+```bash
+pnpm init 
+pnpm next dev 
+```
+
+## License 
+The documentation for Togomak is licensed under the `MPL-2.0` license. 
+This repository contains inherited documentation from [The OpenTofu Project](https://github.com/opentofu/opentofu) which is also licensed under the 
+`MPL-2.0` license.
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,36 @@
+import { useRouter } from "next/router"
+
 export default {
   logo: <span>togomak</span>,
   project: {
     link: 'https://github.com/srevinsaju/togomak'
-  }
+  },
   // ... other theme options
+  docsRepositoryBase: 'https://github.com/srevinsaju/togomak-docs/tree/main',
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Togomak'
+      }
+    }
+  },
+  project: {
+    link: 'https://github.com/srevinsaju/togomak',
+  },
+  footer: {
+    text: (
+      <span>
+      
+      <strong>Togomak</strong> is licensed under MPL-2.0, © {new Date().getFullYear()} {' '}
+        <a href="https://github.com/srevinsaju" target="_blank">
+          <strong>Srevin Saju</strong> 
+        </a> and other contributors.
+        <br></br>
+        <a href="https://terraform.org" target="_blank"><strong>HashiCorp Terraform</strong>, <strong>Terraform</strong></a> and the Terraform Logo are trademarks of <a href="https://hashicorp.com" target="_blank">HashiCorp</a>.
+        <br></br>
+        <a href="https://opentofu.org" target="_blank"><strong>OpenTofu</strong></a> is licensed under MPL-2.0 
+      </span>
+    )
+  }
 }
